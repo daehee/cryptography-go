2c4af9e1982aec9ac212106a81ffe82e150b4bf1
Daehee Park <hello@daehee.com>
2019-09-12 15:47:40 -0700

    s1c3 count spaces

--- s1c3_SingleByteXOR/main.go
+++ s1c3_SingleByteXOR/main.go
@@ -7,28 +7,39 @@ import (
 	"github.com/daehee/cryptography-go/utils"
 )
 
-const MIN_SCORE = 23
+type decoded struct {
+	str   string
+	count int
+}
+
+func findMax(ds []decoded) string {
+	max := ds[0]
+
+	for _, d := range ds {
+		if d.count > max.count {
+			max = d
+		}
+	}
+	return max.str
+}
 
 func main() {
 	data := "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 	b, _ := hex.DecodeString(data)
 
-	for k := 0; k <= 255; k++ {
-		decoded := utils.XorByte(b, byte(k))
-		score := scoreText(decoded)
-		if score > MIN_SCORE {
-			fmt.Println(string(decoded))
-			fmt.Println(score)
-		}
-	}
-}
+	ds := make([]decoded, 256)
 
-func scoreText(a []byte) int {
-	count := 0
-	for _, ch := range a {
-		if ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z' {
-			count++
+	for k := 0; k < 256; k++ {
+		ds[k].str = string(utils.XorByte(b, byte(k)))
+		// count number of spaces
+		for _, v := range ds[k].str {
+			if rune(v) == ' ' {
+				ds[k].count++
+			}
 		}
 	}
-	return count
+
+	res := findMax(ds)
+	fmt.Println(res)
+
 }
